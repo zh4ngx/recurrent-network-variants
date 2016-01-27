d7278f13eb6dd87024eb3c4543aeb18cb3279b94
Andy Zhang <signupforandy@gmail.com>
2016-01-26 19:45:09 -0800

    Move u_g into curr_layer loop; remove U

--- feedback_cell.py
+++ feedback_cell.py
@@ -17,8 +17,6 @@ class FeedbackCell(rnn_cell.MultiRNNCell):
         with tf.variable_scope(scope or type(self).__name__):
             # Conveniently the concatenation of all hidden states at t-1
             h_star_t_prev = state
-            u_g = tf.get_variable("u_g", [self.state_size],
-                                  initializer=tf.random_uniform_initializer(-0.1, 0.1))
             cur_state_pos = 0
             cur_inp = inputs
             new_states = []
@@ -27,12 +25,12 @@ class FeedbackCell(rnn_cell.MultiRNNCell):
                     cur_state = array_ops.slice(
                             state, [0, cur_state_pos], [-1, cell.state_size])
                     with tf.variable_scope("Global Reset"):
+                        u_g = tf.get_variable("u_g", [self.state_size],
+                                              initializer=tf.random_uniform_initializer(-0.1, 0.1))
                         w_g = tf.get_variable("w_g", cell.state_size,
                                               initializer=tf.random_uniform_initializer(-0.1, 0.1))
-                        g = tf.sigmoid(tf.mul(w_g, cur_state) + tf.mul(u_g, h_star_t_prev))
-                        U = tf.get_variable("U", [cell.state_size],
-                                            initializer=tf.random_uniform_initializer(-0.1, 0.1))
-                        cur_state = tf.reduce_sum(g * tf.matmul(cur_state, U))
+                        g = tf.sigmoid(tf.mul(w_g, cur_inp) + tf.mul(u_g, h_star_t_prev))
+                        cur_state = tf.reduce_sum(g * cur_state)
 
                     cur_state_pos += cell.state_size
                     cur_inp, new_state = cell(cur_inp, cur_state)
