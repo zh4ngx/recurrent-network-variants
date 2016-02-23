a6bb8759f33d2745afe6e637ffc0f68d3639c9bf
Andy Zhang <signupforandy@gmail.com>
2016-02-23 00:32:27 -0800

    Collect and input all prev h activations

--- feedback_cell.py
+++ feedback_cell.py
@@ -92,18 +92,20 @@ class FeedbackCell(rnn_cell.MultiRNNCell):
     def layer_sizes(self):
         return self._layer_sizes
 
-    def __call__(self, inputs, state, scope=None):
+    def __call__(self, inputs, hs_prev, state, scope=None):
         with tf.variable_scope(scope or type(self).__name__):
             # Conveniently the concatenation of all hidden states at t-1
             cur_state_pos = 0
             cur_inp = inputs
             new_states = []
+            new_hs = []
             for i, cell in enumerate(self._cells):
                 with tf.variable_scope("Cell%d" % i):
                     cur_state = array_ops.slice(
                             state, [0, cur_state_pos], [-1, cell.state_size])
                     cur_state_pos += cell.state_size
-                    cur_inp, new_state = cell(cur_inp, cur_state, state, self.layer_sizes)
+                    cur_inp, new_state = cell(cur_inp, cur_state, hs_prev, self.layer_sizes)
                     new_states.append(new_state)
+                    new_hs.append(cur_inp)
 
-        return cur_inp, array_ops.concat(1, new_states)
+        return cur_inp, new_hs, array_ops.concat(1, new_states)
